b392da1083d78543c3d228a54b1d733a20324d0f
eisenhowair <elias.mouaheb@gmail.com>
2024-05-15 13:29:33 +0200

    debut triple prompt pour corriger exo

--- modele_personnalise/triple_prompt_exercice.py
+++ modele_personnalise/triple_prompt_exercice.py
@@ -7,7 +7,7 @@ from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
 
 model = Ollama(base_url="http://localhost:11434", model="llama3:8b")
-
+a_corriger = True
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -35,7 +35,7 @@ async def on_chat_start():
 
 
 
-def setup_exercice_model(loisirs):
+def setup_exercice_model():
     """
     Configure le prompt et le Runnable pour générer des exercices de mathématiques personnalisés en fonction des centres d'intérêt de l'utilisateur.
 
@@ -46,6 +46,7 @@ def setup_exercice_model(loisirs):
         None : Met à jour la session utilisateur avec le nouveau Runnable pour générer des exercices.
     """
 
+    loisirs = cl.user_session.get("loisirs")
     niveau_scolaire = "élémentaire"
     prompt_exercice = ChatPromptTemplate.from_messages(
         [
@@ -64,7 +65,29 @@ def setup_exercice_model(loisirs):
     runnable_exercice = prompt_exercice | model | StrOutputParser()
     cl.user_session.set("runnable", runnable_exercice)
 
+def setup_corrige_model(dernier_exo,reponse_user):
+    """
+    Configure le prompt et le Runnable pour corriger des exercices de mathématiques en paramètre.
 
+    Args:
+        dernier_exo (str): Une chaîne représentant le dernier exercice généré.
+        reponse_user (str): Une chaîne pour la réponse soumise par l'utilisateur
+    """
+
+    prompt_corrige = ChatPromptTemplate.from_messages(
+        [
+        (
+            "system",
+            "Tu parles uniquement français. Ton rôle est de corriger l'exercice de mathématiques suivant: "+dernier_exo+". \
+            Si la réponse {question} donnée par l'utilisateur est juste, félicite-le. \
+            Sinon, dis-lui qu'il a faux, et dis-lui la correction de l'exercice."
+        ),
+        ]
+    )
+    
+
+    runnable_corrige = prompt_corrige | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable_corrige)
 
 @cl.on_message
 async def on_message(message: cl.Message):
@@ -78,24 +101,36 @@ async def on_message(message: cl.Message):
     Returns:
         None : Envoie une réponse appropriée à l'utilisateur en fonction du contexte de la conversation.
     """
+    a_corriger != a_corriger
     loisirs = cl.user_session.get("loisirs")
     runnable = cl.user_session.get("runnable")  # type: Runnable
 
     if not loisirs:
-        loisirs = message.content
-        print("loisirs:", loisirs)
+        loisirs = message.content # récupération des loisirs
         cl.user_session.set("loisirs", loisirs)
-        setup_exercice_model(loisirs=loisirs)
+        setup_exercice_model() # préparation du prompt avec loisirs
         response = "Merci! Quel genre d'exercice voulez-vous?"
         await cl.Message(content=response).send()
     else:
-        msg = cl.Message(content="")
-        async for chunk in runnable.astream(
-            {"question": message.content},
-            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-        ):
-            await msg.stream_token(chunk)
-            print(chunk)
-        await msg.send()
-        print(message.content)
-  
+        if not a_corriger: # partie génération d'exercice
+            msg = cl.Message(content="")
+            async for chunk in runnable.astream(
+                {"question": message.content},
+                config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+            ):
+                await msg.stream_token(chunk)
+                dernier_exo += chunk
+                print(chunk)
+            await msg.send()
+            print(message.content)
+        else: # partie correction d'exercice
+            msg = cl.Message(content="")
+            async for chunk in runnable.astream(
+                {"question": message.content},
+                config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+            ):
+                await msg.stream_token(chunk)
+                dernier_exo += chunk
+                print(chunk)
+            await msg.send()
+            print(message.content)
