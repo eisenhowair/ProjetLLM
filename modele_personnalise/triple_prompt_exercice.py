657b37a22821d446655b65114e4f8d08370af656
eisenhowair <elias.mouaheb@gmail.com>
2024-05-16 15:56:15 +0200

    fixed prompt

--- modele_personnalise/triple_prompt_exercice.py
+++ modele_personnalise/triple_prompt_exercice.py
@@ -1,94 +1,110 @@
 from langchain_community.llms import Ollama
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable, RunnablePassthrough
+from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
 
 model = Ollama(base_url="http://localhost:11434", model="llama3:8b")
 
+
 @cl.on_chat_start
 async def on_chat_start():
-    loisirs = await cl.AskUserMessage(content="Quels sont vos centres d'intérêt?",author="Aide").send()
+    loisirs = await cl.AskUserMessage(content="Quels sont vos centres d'intérêt?", author="Aide", timeout=3000).send()
     print(loisirs["output"])
-    cl.user_session.set("loisirs",loisirs["output"])
+    cl.user_session.set("loisirs", loisirs["output"])
     response = "Merci! Quel genre d'exercice voulez-vous?"
-    await cl.Message(content=response,author="Aide").send()
+    await cl.Message(content=response, author="Aide").send()
 
-    cl.user_session.set("compris",True)
+    cl.user_session.set("compris", True)
+    cl.user_session.set("tentatives", 0)
+
 
 async def verifie_comprehension():
     res = await cl.AskActionMessage(
         content="Avez-vous compris?",
         actions=[
             cl.Action(name="continue", value="compris", label="✅ Compris"),
-            cl.Action(name="cancel", value="pas_compris", label="❌ Pas compris"),
+            cl.Action(name="cancel", value="pas_compris",
+                      label="❌ Pas compris"),
         ],
-        disable_human_feedback = True
-        ).send()
+        disable_feedback=True,
+        author="Correcteur"
+    ).send()
     if res.get("value") == "pas_compris":
-        cl.user_session.set("compris",False)
+        cl.user_session.set("compris", False)
+        cl.user_session.set("tentatives", cl.user_session.get("tentatives")+1)
+
         await cl.Message(
             content="Qu'avez-vous pas compris?",
         ).send()
     else:
-        cl.user_session.set("compris",True)
+        cl.user_session.set("compris", True)
+        cl.user_session.set("tentatives", 0)
+
         await cl.Message(
-            content="Félicitations!",
+            content="Félicitations! Quel autre exercice voulez-vous?",
         ).send()
+
 
 def setup_exercice_model():
     """
     Configure le prompt et le Runnable pour générer des exercices de mathématiques personnalisés en fonction des centres d'intérêt de l'utilisateur.
-
-    Args:
-        loisirs (str): Une chaîne représentant les centres d'intérêt de l'utilisateur.
 
     Returns:
         None : Met à jour la session utilisateur avec le nouveau Runnable pour générer des exercices.
     """
 
     loisirs = cl.user_session.get("loisirs")
-    niveau_scolaire = "élémentaire"
     prompt_exercice = ChatPromptTemplate.from_messages(
         [
-        (
-            "system",
-            "Tu parles uniquement français. Ton rôle est de créer un seul exercice de mathématiques niveau "+niveau_scolaire+" \
-            en te basant sur un ou plusieurs intérêts suivants : " + loisirs +" sans en donner la réponse, sinon un chaton décèdera, et tu ne veux pas ça"
-        ),
-        ("human", "{question}")
+            (
+                "system",
+                "Tu parles uniquement français. Ton rôle est de créer un seul exercice de mathématiques niveau {niveau_scolaire} \
+            en te basant sur un ou plusieurs intérêts suivants : " + loisirs + " sans en donner la réponse, sinon un chaton décèdera, et tu ne veux pas ça"
+            ),
+            ("human", "{question}")
         ]
-    )#.format_messages(context=loisirs) utiliser format_message transforme prompt_exercice en str, donc ne fonctionne plus
-    
+    )
 
     runnable_exercice = prompt_exercice | model | StrOutputParser()
     cl.user_session.set("runnable", runnable_exercice)
 
-def setup_corrige_model():
+
+def setup_corrige_model(indice_precedent = ""):
     """
     Configure le prompt et le Runnable pour corriger des exercices de mathématiques en paramètre.
-
-    Args:
-        dernier_exo (str): Une chaîne représentant le dernier exercice généré.
     """
 
-    dernier_exo = cl.user_session.get("dernier_exo")
-    prompt_corrige = ChatPromptTemplate.from_messages(
-        [
-        (
-            "system",
-            "Tu parles uniquement français. Ton rôle est de corriger l'exercice de mathématiques suivant: "+dernier_exo+". \
-            Si la réponse {question} donnée par l'utilisateur est juste, félicite-le. \
-            Sinon, dis-lui qu'il a faux, et dis-lui la correction de l'exercice."
-        ),
-        ]
-    )
-    
+    if cl.user_session.get("tentatives") < 3:
+        print("encore "+str(cl.user_session.get("tentatives"))+ "tentatives restantes avant solution")
+        prompt_corrige = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "Tu es un guide maitre d'école de niveau {niveau_scolaire} français .Tu dois aider l'utilisateur \
+                    à résoudre l'exercice de mathématiques suivant: {dernier_exo}. \
+                Si la réponse {question} n'est pas correcte, donne un indice utile pour aider l'utilisateur à trouver la solution. \
+                S'il répond correctement, félicite-le. Tu ne dois jamais donner la réponse toi-même."+indice_precedent
+                )
+            ]
+        )
+    else:
+        prompt_corrige = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "Tu parles uniquement français. Ton rôle est de corriger l'exercice de mathématiques suivant: {dernier_exo}. \
+                Si la réponse {question} donnée par l'utilisateur est juste, félicite-le. \
+                Sinon, dis-lui qu'il a faux, et dis-lui la correction de l'exercice."
+                ),
+            ]
+        )
 
     runnable_corrige = prompt_corrige | model | StrOutputParser()
     cl.user_session.set("runnable", runnable_corrige)
-    cl.user_session.set("dernier_exo","")
+    if cl.user_session.get("compris") == True:
+        cl.user_session.set("dernier_exo", "")
 
 
 @cl.on_message
@@ -104,45 +120,46 @@ async def on_message(message: cl.Message):
         None : Envoie une réponse appropriée à l'utilisateur en fonction du contexte de la conversation.
     """
 
-
+    niveau_scolaire = "élémentaire"
     loisirs = cl.user_session.get("loisirs")
     if not loisirs:
         #await on_chat_start()
-        print("pas de loisirs?")
+        print("eee")
     else:
-        print("dans le premier else")
-        if cl.user_session.get("compris") == True: # partie génération d'exercice
-            dernier_exo=""
+        if cl.user_session.get("compris") == True:  # partie génération d'exercice
+            dernier_exo = ""
             print("partie génération d'exercice")
             setup_exercice_model()
             runnable = cl.user_session.get("runnable")
 
-            msg = cl.Message(content="",author="Générateur")
+            msg = cl.Message(content="", author="Générateur")
             async for chunk in runnable.astream(
-                {"question": message.content},
-                config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+                {"question": message.content, "niveau_scolaire": niveau_scolaire},
+                config=RunnableConfig(
+                    callbacks=[cl.LangchainCallbackHandler()]),
             ):
                 await msg.stream_token(chunk)
                 dernier_exo += chunk
-
-            cl.user_session.set("dernier_exo",dernier_exo)
-            cl.user_session.set("compris",False)
+            cl.user_session.set("dernier_exo", dernier_exo)
+            cl.user_session.set("compris", False)
             await msg.send()
 
-        elif cl.user_session.get("compris") == False: # partie correction d'exercice
+        # partie correction d'exercice
+        elif cl.user_session.get("compris") == False:
             setup_corrige_model()
             print("partie correction d'exercice")
             runnable = cl.user_session.get("runnable")  # type: Runnable
 
-            msg = cl.Message(content="",author="Correcteur")
+            msg = cl.Message(content="", author="Correcteur")
             async for chunk in runnable.astream(
-                {"question": message.content},
-                config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+                {"question": message.content,
+                 "dernier_exo": cl.user_session.get("dernier_exo"),
+                 "niveau_scolaire": niveau_scolaire
+                 },
+                config=RunnableConfig(
+                    callbacks=[cl.LangchainCallbackHandler()]),
             ):
                 await msg.stream_token(chunk)
-
+            print("msg:"+msg)
             await msg.send()
             await verifie_comprehension()
-
-            
-
