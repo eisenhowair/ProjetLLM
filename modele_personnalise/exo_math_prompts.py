d81f190ce93ba8377307e9fbfb05a457246a5fca
eisenhowair <elias.mouaheb@gmail.com>
2024-05-17 15:35:46 +0200

    now login+history+literalAI to follow threads

--- modele_personnalise/exo_math_prompts.py
+++ modele_personnalise/exo_math_prompts.py
@@ -1,11 +1,18 @@
 from langchain_community.llms import Ollama
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 import chainlit as cl
 from chainlit.input_widget import Slider
+from operator import itemgetter
 
+
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.schema.runnable.config import RunnableConfig
+from langchain.memory import ConversationBufferMemory
+
+from chainlit.types import ThreadDict
 
 model = Ollama(base_url="http://localhost:11434", model="llama3:instruct")
 
@@ -14,12 +21,13 @@ model = Ollama(base_url="http://localhost:11434", model="llama3:instruct")
 def auth_callback(username: str, password: str):
     # Fetch the user matching username from your database
     # and compare the hashed password with the value stored in the database
-    if (username, password) == ("admin", "admin"):
+    if (username, password) == ("elias", "elias"):
         return cl.User(
-            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+            identifier="Elias", metadata={"role": "admin", "provider": "credentials"}
         )
     else:
         return None
+
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -38,16 +46,20 @@ async def on_chat_start():
     ).send()
 
     loisirs = await cl.AskUserMessage(content="Quels sont vos centres d'intérêt?", author="Aide", timeout=3000).send()
-    #print(loisirs["output"])
+    # print(loisirs["output"])
     cl.user_session.set("loisirs", loisirs["output"])
     response = "Merci! Quel genre d'exercice voulez-vous?"
     await cl.Message(content=response, author="Aide").send()
 
     cl.user_session.set("compris", True)
     cl.user_session.set("tentatives", 0)
+    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+
 
 
 async def verifie_comprehension():
+
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
     res = await cl.AskActionMessage(
         content="Avez-vous compris?",
         actions=[
@@ -59,22 +71,31 @@ async def verifie_comprehension():
         author="Correcteur",
         timeout=3000
     ).send()
+
+
+
     if res.get("value") == "pas_compris":
         cl.user_session.set("compris", False)
         cl.user_session.set("tentatives", cl.user_session.get("tentatives")+1)
 
-        await cl.Message(
+        msg = await cl.Message(
             content="Qu'avez-vous pas compris?",
         ).send()
     else:
         cl.user_session.set("compris", True)
         cl.user_session.set("tentatives", 1)
 
-        await cl.Message(
+        msg = await cl.Message(
             content="Félicitations! Quel autre exercice voulez-vous?",
         ).send()
+    
+    # on met à jour l'historique
+    memory.chat_memory.add_ai_message("Avez-vous compris?")
+    memory.chat_memory.add_user_message(res.get("value"))
+    memory.chat_memory.add_ai_message(msg.content)
 
-@cl.step(type="run",name="runnable_generation")
+
+@cl.step(type="run", name="runnable_generation")
 def setup_exercice_model():
     """
     Configure le prompt et le Runnable pour générer des exercices de mathématiques personnalisés en fonction des centres d'intérêt de l'utilisateur.
@@ -83,6 +104,7 @@ def setup_exercice_model():
         None : Met à jour la session utilisateur avec le nouveau Runnable pour générer des exercices.
     """
 
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
     loisirs = cl.user_session.get("loisirs")
     prompt_exercice = ChatPromptTemplate.from_messages(
         [
@@ -91,28 +113,42 @@ def setup_exercice_model():
                 "Tu parles uniquement français. Ton rôle est de créer un seul exercice de mathématiques \
             en te basant sur un ou plusieurs intérêts suivants : " + loisirs + ". L'exercice doit impliquer :{question}, et être du niveau d'un élève ayant {niveau_scolaire}"
             ),
+            MessagesPlaceholder(variable_name="history"),
             ("human", "{question}")
         ]
     )
 
-    runnable_exercice = prompt_exercice | model | StrOutputParser()
+    runnable_exercice = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(
+                memory.load_memory_variables) | itemgetter("history")
+        ) 
+        | prompt_exercice
+        | model
+        | StrOutputParser()
+    )
     cl.user_session.set("runnable", runnable_exercice)
     return runnable_exercice
 
-@cl.step(type="run",name="runnable_corrige")
-def setup_corrige_model(indice_precedent = ""):
+
+@cl.step(type="run", name="runnable_corrige")
+def setup_corrige_model():
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
     if cl.user_session.get("tentatives") < 3:
         print("partie aide d'exercice")
-        print("Nombre de tentatives faites: "+str(cl.user_session.get("tentatives")))
+        print("Nombre de tentatives faites: " +
+              str(cl.user_session.get("tentatives")))
         prompt_corrige = ChatPromptTemplate.from_messages(
             [
                 (
                     "system",
                     "Tu es un maitre d'école avec des enfants de {niveau_scolaire} français .Tu dois aider l'utilisateur \
                     à résoudre l'exercice de mathématiques suivant: {dernier_exo}. \
-                Si la réponse {question} n'est pas correcte, donne un indice utile pour aider l'utilisateur à trouver la solution. \
-                S'il répond correctement, félicite-le. Tu ne dois jamais donner la réponse toi-même."+indice_precedent
-                )
+                Si la réponse de l'utilisateur n'est pas correcte, donne un indice utile pour aider l'utilisateur à trouver la solution. \
+                S'il répond correctement, félicite-le. Tu ne dois jamais donner la réponse toi-même."
+                ),
+                MessagesPlaceholder(variable_name="history"),
+                ("human", "{question}")
             ]
         )
     else:
@@ -125,14 +161,22 @@ def setup_corrige_model(indice_precedent = ""):
                 Si la réponse {question} donnée par l'utilisateur est juste, félicite-le. \
                 Sinon, dis-lui qu'il a faux, et dis-lui la correction de l'exercice."
                 ),
+                MessagesPlaceholder(variable_name="history"),
+                ("human", "{question}")
             ]
         )
 
-    runnable_corrige = prompt_corrige | model | StrOutputParser()
+    runnable_corrige = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+            )
+            | prompt_corrige
+            | model 
+            | StrOutputParser())
     cl.user_session.set("runnable", runnable_corrige)
     if cl.user_session.get("compris") == True:
         cl.user_session.set("dernier_exo", "")
-    
+
     return runnable_corrige
 
 
@@ -148,9 +192,10 @@ async def on_message(message: cl.Message):
     Returns:
         None : Envoie une réponse appropriée à l'utilisateur en fonction du contexte de la conversation.
     """
+    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
 
     if cl.user_session.get("age_niveau"):
-        niveau_scolaire= str(cl.user_session.get("age_niveau"))+" ans"
+        niveau_scolaire = str(cl.user_session.get("age_niveau"))+" ans"
     else:
         niveau_scolaire = "5 ans"
 
@@ -158,7 +203,6 @@ async def on_message(message: cl.Message):
         dernier_exo = ""
         print("partie génération d'exercice")
         runnable = setup_exercice_model()
-        #runnable = cl.user_session.get("runnable")
 
         msg = cl.Message(content="", author="Générateur")
         async for chunk in runnable.astream(
@@ -172,27 +216,59 @@ async def on_message(message: cl.Message):
         cl.user_session.set("compris", False)
         await msg.send()
 
+        memory.chat_memory.add_user_message(message.content)
+        memory.chat_memory.add_ai_message(msg.content)
+
         # partie correction d'exercice
     elif cl.user_session.get("compris") == False:
         runnable = setup_corrige_model()
-        #runnable = cl.user_session.get("runnable")  # type: Runnable
 
         msg = cl.Message(content="", author="Correcteur")
         async for chunk in runnable.astream(
             {"question": message.content,
                 "dernier_exo": cl.user_session.get("dernier_exo"),
                 "niveau_scolaire": niveau_scolaire
-                },
+             },
             config=RunnableConfig(
                 callbacks=[cl.LangchainCallbackHandler()]),
         ):
             await msg.stream_token(chunk)
         print("msg:"+str(msg.content))
         await msg.send()
+        memory.chat_memory.add_user_message(message.content)
+        memory.chat_memory.add_ai_message(msg.content)
         await verifie_comprehension()
-
 
 
 @cl.on_settings_update
 async def setup_agent(settings):
-    cl.user_session.set("age_niveau",settings['age_cible'])
+    cl.user_session.set("age_niveau", settings['age_cible'])
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    memory = ConversationBufferMemory(return_messages=True)
+    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
+    for message in root_messages:
+        if message["type"] == "user_message":
+            memory.chat_memory.add_user_message(message["output"])
+        else:
+            memory.chat_memory.add_ai_message(message["output"])
+
+    cl.user_session.set("memory", memory)
+
+    await cl.ChatSettings(
+        [
+            Slider(
+                id="age_cible",
+                label="Age niveau exercice",
+                initial=8,
+                min=3,
+                max=22,
+                step=1,
+                tooltip="en années",
+            ),
+        ]
+    ).send()
+
+    setup_exercice_model()
