c149cd6e97162364210f6d5b7a472e98ef4280ef
Elias Mouaheb <elias.mouaheb@gmail.com>
2024-05-17 11:50:53 +0200

    improved prompt math file

--- modele_personnalise/exo_math_prompts.py
+++ modele_personnalise/exo_math_prompts.py
@@ -14,14 +14,6 @@ model = Ollama(base_url="http://localhost:11434", model="llama3:instruct", miros
 async def on_chat_start():
     settings = await cl.ChatSettings(
         [
-            TextInput(id="AgentName", label="Agent Name", initial="AI"),
-            Select(
-                id="Model",
-                label="OpenAI - Model",
-                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
-                initial_index=0,
-            ),
-            Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
             Slider(
                 id="age_cible",
                 label="Age niveau exercice",
