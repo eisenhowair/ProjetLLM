5e594ed982e4569e1241c668863802351ac3bab5
eisenhowair <elias.mouaheb@gmail.com>
2024-05-30 15:00:50 +0200

    fetch github info

--- PR_Reviewer/github_recup.py
+++ PR_Reviewer/github_recup.py
@@ -1,3 +1,11 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings, load_index_from_storage
+from llama_index.readers.github import GithubRepositoryReader, GithubClient
+from IPython.display import Markdown, display
+from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+import faiss
 import os
 from dotenv import load_dotenv
 
@@ -7,18 +15,70 @@ load_dotenv(dotenv_path=env_path)
 REPO_NAME = os.getenv("REPO_NAME")
 REPO_OWNER = os.getenv("REPO_OWNER")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+BRANCH = "main"
 
-# Initialize the tool for semantic searches within a specific GitHub repository
-tool = GithubSearchTool(
-    github_repo='https://github.com/{REPO_OWNER}/{REPO_NAME}',
-    content_types=['code', 'issue']  # Options: code, repo, pr, issue
+github_client = GithubClient(github_token=GITHUB_TOKEN, verbose=True)
+
+documents = GithubRepositoryReader(
+    github_client=github_client,
+    owner=REPO_OWNER,
+    repo=REPO_NAME,
+    use_parser=False,
+    verbose=False,
+    filter_directories=(
+        ["sandboxRAG/differents_textes", "sandboxRAG2"],
+        GithubRepositoryReader.FilterType.EXCLUDE,
+    ),
+    filter_file_extensions=(
+        [
+            ".png",
+            ".txt",
+            ".jpg",
+            ".jpeg",
+            ".py",
+            ".md",
+            "json",
+            ".ipynb",
+        ],
+        GithubRepositoryReader.FilterType.INCLUDE,
+    ),
+).load_data(branch=BRANCH)
+print(documents)
+###
+embed_model = HuggingFaceEmbedding(
+    model_name="sentence-transformers/all-mpnet-base-v2",
 )
+llm = Ollama(base_url="http://localhost:11434",
+             model="llama3:instruct", request_timeout=1000.0)
 
-# OR
+d = 768
+faiss_index = faiss.IndexFlatL2(d)
 
-# Initialize the tool for semantic searches within a specific GitHub repository, so the agent can search any repository if it learns about during its execution
-tool = GithubSearchTool(
-    content_types=['code', 'issue']  # Options: code, repo, pr, issue
+Settings.llm = llm
+Settings.embed_model = embed_model
+Settings.node_parser = SentenceSplitter(chunk_size=512, chunk_overlap=20)
+Settings.num_output = 512
+Settings.context_window = 3900
+
+vector_store = FaissVectorStore(faiss_index=faiss_index)
+storage_context_global = StorageContext.from_defaults(
+    vector_store=vector_store)
+
+index = VectorStoreIndex.from_documents(
+    documents, storage_context=storage_context_global, show_progress=True)
+index.storage_context.persist("index_github")
+query_engine = index.as_query_engine()
+response = query_engine.query(
+    "Quelles sont les différences entre un RAG langchain et un RAG llama_index?",
+    # verbose=True,
 )
+print(response.response)
 
-print(tool)
+# display(Markdown(f"{response}"))
+
+"""
+https://docs.llamaindex.ai/en/stable/examples/data_connectors/GithubRepositoryReaderDemo/ (doc officielle)
+https://github.com/joaomdmoura/crewAI
+https://docs.crewai.com/tools/GitHubSearchTool (impossible d'importer crewAI)
+https://lightning.ai/lightning-ai/studios/chat-with-your-code-using-rag (a donné des pistes)
+"""
