895f3d642e0db1306d7d7608100744e2f8b2b3e3
Théo <117837505+TheoNicod@users.noreply.github.com>
2024-06-10 19:13:32 +0200

    Changement prompt

--- ollama-reply-zimbra/dist/assets/src/entries/background/main-D31HeJcN.js
+++ ollama-reply-zimbra/dist/assets/src/entries/background/main-D31HeJcN.js
@@ -7,13 +7,15 @@ n.runtime.onInstalled.addListener(()=>{
 	n.runtime.onMessage.addListener((e,s,t)=> {
     console.log(e.tweet);
 
-    let r = `Vous êtes un assistant IA français. Vous devez répondre au e-mails sur un ton ${e.ton}. `;
-
-    if (e.indications) {
-      r += `En suivant les indications.
-      Indications :
-      ${e.indications}`;
-    }
+	let r = `
+ 
+ 	Vous êtes un assistant IA français spécialisé dans la gestion des e-mails. Répondez aux e-mails en utilisant un ton ${e.ton}. Veuillez suivre les instructions suivantes :
+	1. Respectez les indications fournies : ${e.indications}.
+	2. Adaptez votre langage et votre style en fonction du destinataire de l'e-mail.
+	3. Assurez-vous que toutes les réponses soient polies, professionnelles et respectueuses.
+	4. Relisez votre réponse pour vérifier la grammaire et l'orthographe avant de l'envoyer.
+	5. Fournissez des informations supplémentaires si nécessaire pour clarifier ou enrichir la réponse.
+	`
 
 		if(e.action==="fetchResponse")
 			return i(e.tweet, r).then(a=>{
